41679be1733d94a5454ae16484f5e716898db803
Wes Williams <wewilliams@paypal.com>
2019-04-02 13:40:04 -0700

    new middle ware helmet, cors, compression

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,35 @@
 import express from 'express';
 import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
 
 const app = express();
 const root = path.join(__dirname, '../../');
 
+// routing
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(root, '/dist/client/index.html'));
 });
+
+// http security
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+    imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+  }
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+
+// croos origin resource shaing
+app.use(cors());
+
+//compress
+app.use(compress());
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
