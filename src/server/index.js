26339a307be311d3d23093d8d415e0b01c0e9866
Wes Williams <wewilliams@paypal.com>
2019-04-02 11:26:16 -0700

    initial commit

---
+++ src/server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const root = path.join(__dirname, '../../');
+
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(root, '/dist/client/index.html'));
+});
+
+app.listen(8000, () => console.log('Listening on port 8000!'));
